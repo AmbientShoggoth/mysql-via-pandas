d3b146267c6ce3b896e0d5ca722708e9070bd854
Shoggoth Prime <stbroomby@gmail.com>
2021-11-18 15:03:42 +0000

    Finished first ver of credentials.py, updated sharepoint.py to account for changes.

--- src/credentials.py
+++ src/credentials.py
@@ -4,29 +4,28 @@ from sys import argv,exit as sys_exit
 from platform import system as get_system
 
 def main():
-    
+    # get run.py directory
     parent_dir=os_path.dirname(os_path.realpath(__file__)).split("mysql-via-pandas")[0]+"mysql-via-pandas/"
     
     def config_error(err_info,err_code=1):
-        print(f"\nThere's an issue with your config.ini: {err_info}\n")
+        input(f"\nThere's an issue with your config.ini: {err_info}\n")
         sys_exit(err_code)
-
+    
     def config_type_string_to_dict(in_string):
+        """Convert contents of ini files to dict"""
         string_list=in_string.split("\n")
         string_list=[i.split("#")[0].replace(" ","").split("=") for i in string_list if i and "=" in i]
         return({pair[0]:pair[1] for pair in string_list})
         
-
     # Read config file
     try:
         with open(f"{parent_dir}config.ini","r") as f:
             config_string=f.read()
     except FileNotFoundError:config_error("config.ini missing")
-    
-    # Filter out blank lines, comments, spaces, and convert to dict
     config_dict=config_type_string_to_dict(config_string)
     
     if not config_dict:config_error("no valid content.")
+    
     
     if os_path.isfile(f"{parent_dir}/data/safe_credentials.dat"):
         print("Attempting to use encrypted credentials.")
@@ -37,64 +36,27 @@ def main():
             with open(f"{parent_dir}credentials.ini","r") as f:
                 credentials_string=f.read()
         except FileNotFoundError:
-            print("Unable to find safe_credentials.dat or credentials.ini")
-        
+            input("Unable to find safe_credentials.dat or credentials.ini")
+            sys_exit(2)
+    
     credentials_dict=config_type_string_to_dict(credentials_string)
-    for i,j in credentials_dict.items():print(f"{i}: {j}")
-    
-    """
-    try:env_vars_toggle=int(config_dict["use_env_vars"])
-    except ValueError:
-        print("env_vars_toggle should be either 1 or 0. Assuming 0")
-        env_vars_toggle=False # 
-    except KeyError:config_error("use_env_vars missing")""";
-    
-    
-    
-        
-    #if int(config_dict["use_env_vars"]): # use env variables
-        
-    
-
-
-    #os_path.dirname(os_path.realpath(__file__)) # py script directory
-    
-    """
-    platform_string=get_system()
-    if platform_string=="Windows":split_char=";" # windows
-    else:split_char=":" # macos, linux""";
-
-    print("Yes, indeed")
-    
-    
-    
-    """
+    global cred
     class cred:
-        env_vars=os_environ.get("lvnvariables")
-        username,app_password,user,host,port,schema,site_url,start_path=env_vars.split(";")
-        URI=f"mysql://{user}@{host}:{port}/{schema}"
-        full_url=site_url+start_path
-        winuser=os_environ.get("username")
-    """;
-    """
-    class cred:
-        env_vars=os_environ.get("lvnvariables")
-        username,app_password,user,host,port,schema,site_url,start_path=env_vars.split(";")
-        URI=f"mysql://{user}@{host}:{port}/{schema}"
-        full_url=site_url+start_path
-        winuser=os_environ.get("username")
-
-    elif system()=="Darwin":#macos
-        class cred:
-            lvnvars=os_environ.get("lvnvariables")
-            username,app_password,user,host,port,schema,site_url,start_path=lvnvars.split(":")
+        class sql:
+            user=credentials_dict["sql_user"]
+            password=credentials_dict["sql_user_password"]
+            db_ip=credentials_dict["sql_db_ip"]
+            db_port=credentials_dict["sql_db_port"]
+            db_schema=credentials_dict["sql_db_schema"]
             
-            user=user.replace("£",":")
-            site_url=site_url.replace("£",":")
-            
-            URI=f"mysql://{user}@{host}:{port}/{schema}"
-            full_url=site_url+start_path
-            #winuser=os_environ.get("username")
-    """;
+        class sp:
+            if config_dict["enable_sharepoint_uploads"]=="1":
+                enabled=True
+                username=credentials_dict["sp_username"]
+                app_password=credentials_dict["sp_app_password"]
+                site_url=credentials_dict["sp_site_url"]
+                full_url=site_url+credentials_dict["sp_start_path"]
+            else:enabled=False
+    return(cred)
     
 if __name__ == '__main__':main()
--- src/sharepoint.py
+++ src/sharepoint.py
@@ -1,12 +1,14 @@
 from shareplum import Office365,site as sp_site,Site
 import shareplum as sp
 
-def init(site_url,dir_url,sp_username,app_password):
+def init(cred_sp):
     """ Initiate module through creation of a shareplum.site._Site365 object. Requires site url, base directory url, username and password."""
-    authcookie = Office365(site_url, username=sp_username, password=app_password).GetCookies()
-    version=sp_site.Version.v365
-    global site
-    site = Site(dir_url, version=version, authcookie=authcookie)
+    if cred_sp.enabled:
+        authcookie = Office365(cred_sp.site_url, username=cred_sp.username, password=cred_sp.app_password).GetCookies()
+        version=sp_site.Version.v365
+        global site
+        site = Site(dir_url, version=version, authcookie=authcookie)
+    return(cred_sp.enabled)
 
 def get_folder(end_path):
     """Returns shareplum.Site.Folder object using input path."""
